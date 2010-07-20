597cb141e114da36045ecb7f4b350feb389c0c33
conrad.john <conrad.john@gmail.com>
2010-07-20 05:54:39 +0000

    Initial commit. EXTREMELY ROUGH and not ready for any kind of use, but committed for collaboration purposes.

---
+++ MusicBoxLib/XmlRequests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PandoraMusicBox.Engine {
+    class XmlRequests {
+        public const string AuthenticateListener =
+            "<?xml version=\"1.0\"?><methodCall>" +
+            "<methodName>listener.authenticateListener</methodName>" +
+            "<params><param><value><int>{0}</int></value></param>" +
+            "<param><value><string>{1}</string></value></param>" +
+            "<param><value><string>{2}</string></value></param>" +
+            "</params></methodCall>";
+    }
+}
