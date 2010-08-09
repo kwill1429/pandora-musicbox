abd3bfbc0af559e85997b35c0d55a3dc4001d4fb
travistx@gmail.com <travistx@gmail.com@bb047c8b-32c9-1db6-8714-e51fcf91b571>
2010-08-09 05:16:38 +0000

    Added a "working" animation Made advertisement images keep their original aspect ratio to avoid the drastic stretching Modified merge.bat to merge to a subfolder

--- trunk/Source/MediaPortalPlugin/Resources/merge.bat
+++ trunk/Source/MediaPortalPlugin/Resources/merge.bat
@@ -1,13 +1,3 @@
 @echo off
-
-md tmp
-ilmerge  /out:tmp\PandoraMusicBox.MediaPortalPlugin.dll PandoraMusicBox.MediaPortalPlugin.dll PandoraMusicBox.Engine.dll NLog.dll
-
-IF EXIST PandoraMusicBox.MediaPortalPlugin_UNMERGED.exe del PandoraMusicBox.MediaPortalPlugin_UNMERGED.exe
-ren PandoraMusicBox.MediaPortalPlugin.exe PandoraMusicBox.MediaPortalPlugin_UNMERGED.exe
-IF EXIST PandoraMusicBox.MediaPortalPlugin_UNMERGED.pdb del PandoraMusicBox.MediaPortalPlugin_UNMERGED.pdb
-ren PandoraMusicBox.MediaPortalPlugin.pdb PandoraMusicBox.MediaPortalPlugin_UNMERGED.pdb
-
-move tmp\*.* .
-rd tmp
-
+md merged
+ilmerge  /out:merged\PandoraMusicBox.MediaPortalPlugin.dll PandoraMusicBox.MediaPortalPlugin.dll PandoraMusicBox.Engine.dll NLog.dll
