f8f23dc5780689ade5f4d52fdf7688c8fc6f9aac
conrad.john <conrad.john@bb047c8b-32c9-1db6-8714-e51fcf91b571>
2011-12-10 18:43:46 +0000

    tag for 1.2.3 mepo release

---
+++ 1.2/1.2.3/Source/Engine/Data/PandoraStation.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace PandoraMusicBox.Engine.Data {
+    public class PandoraStation : PandoraData {
+        public string Name {
+            get;
+            internal set;
+        }
+
+        public string Id {
+            get;
+            internal set;
+        }
+
+        public bool IsCreatedByUser {
+            get;
+            internal set;
+        }
+
+        public bool IsQuickMix {
+            get;
+            internal set;
+        }
+        
+        internal PandoraStation(Dictionary<string, string> variables) {
+            this.Variables = variables;
+        }
+
+        internal static List<PandoraStation> Parse(string xmlStr) {
+            List<PandoraStation> stations = new List<PandoraStation>();
+
+            XmlDocument xml = new XmlDocument();
+            xml.LoadXml(xmlStr);
+
+            // loop through each station in the XML document
+            foreach (XmlNode currStationNode in xml.SelectNodes("//methodResponse/params/param/value/array/data/value/struct")) {
+                Dictionary<string, string> varLookup = GetVariables(currStationNode);
+                PandoraStation station = new PandoraStation(varLookup);
+                station.Name = station["stationName"];
+                station.Id = station["stationId"];
+                station.IsCreatedByUser = station["isCreator"] == "1";
+                station.IsQuickMix = station["isQuickMix"] == "1";
+
+                stations.Add(station);
+            }
+
+            return stations;
+        }
+    }
+}
