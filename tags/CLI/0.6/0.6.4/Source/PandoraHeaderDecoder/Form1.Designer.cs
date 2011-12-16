184c16fb7febf5850f5574c7afcaa6e2f578da73
conrad.john <conrad.john@bb047c8b-32c9-1db6-8714-e51fcf91b571>
2011-12-16 04:25:06 +0000

    tag for cli 0.6.4

---
+++ tags/CLI/0.6/0.6.4/Source/PandoraHeaderDecoder/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace PandoraHeaderDecoder {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(13, 13);
+            this.inputTextBox.Multiline = true;
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(523, 178);
+            this.inputTextBox.TabIndex = 0;
+            this.inputTextBox.TextChanged += new System.EventHandler(this.inputTextBox_TextChanged);
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Location = new System.Drawing.Point(13, 198);
+            this.outputTextBox.Multiline = true;
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.ReadOnly = true;
+            this.outputTextBox.Size = new System.Drawing.Size(523, 197);
+            this.outputTextBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(548, 407);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.inputTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.TextBox outputTextBox;
+    }
+}
+
---
+++ tags/CLI/0.6/0.6.4/Source/TrayApp/MusicBoxTray.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace TrayApp {
+    public class MusicBoxTray: Form {
+        protected NotifyIcon trayIcon;
+        protected ContextMenuStrip trayMenu;
+
+        public MusicBoxTray() {
+            // Create a simple tray menu with only one item.
+            trayMenu = new ContextMenuStrip();
+
+            Panel p = new Panel();
+            Label l = new Label();
+            l.Text = "SONG PANEL";
+            l.AutoSize = false;
+            l.Width = 500;
+            p.Controls.Add(l);
+
+            trayMenu.Items.Add(new ToolStripControlHost(p));
+            trayMenu.Items.Add("Exit", null, OnExit);
+
+            // Create a tray icon. In this example we use a
+            // standard system icon for simplicity, but you
+            // can of course use your own custom icon too.
+            trayIcon = new NotifyIcon();
+            trayIcon.Text = "Pandora MusicBox";
+            trayIcon.Icon = new Icon(SystemIcons.Application, 40, 40);
+
+            // Add menu to tray icon and show it.
+            trayIcon.ContextMenuStrip = trayMenu;
+            trayIcon.Visible = true;
+
+            trayIcon.Click += new EventHandler(trayIcon_Click);
+
+        }
+
+        void trayIcon_Click(object sender, EventArgs e) {
+            //Visible = true;
+        }
+
+        protected override void OnLoad(EventArgs e) {
+            Visible = false;
+            ShowInTaskbar = false;
+            base.OnLoad(e);
+        }
+
+        protected void OnExit(object sender, EventArgs e) {
+            Application.Exit();
+        }
+        
+        protected void OnLogin(object sender, EventArgs e) {
+            MessageBox.Show("login");
+        }
+
+    }
+}
