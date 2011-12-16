55f31fb86a7c889588c477ba244cf8334fc0ed61
conrad.john <conrad.john@bb047c8b-32c9-1db6-8714-e51fcf91b571>
2011-12-16 04:25:06 +0000

    tag for cli 0.6.4

---
+++ 0.6/0.6.4/Source/Engine/Encryption/PandoraCryptKeys.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PandoraMusicBox.Engine.Encryption {
+    public partial class PandoraCryptKeys {
+        public static BlowfishKey In;
+        public static BlowfishKey Out;
+        public static BlowfishKey PW;
+
+        static PandoraCryptKeys() {
+            initializeInKey();
+            initializeOutKey();
+            initializePasswordKey();
+        }
+    }
+}
