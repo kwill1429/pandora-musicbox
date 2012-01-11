291c431b0a27eac4bc128c7ccb1ef55c5bee3ff9
conrad.john <conrad.john@bb047c8b-32c9-1db6-8714-e51fcf91b571>
2012-01-11 05:13:28 +0000

    tag for mepo plugin 1.2.6

---
+++ 1.2/1.2.6/Source/MediaPortalPlugin/Tools/BaseSettings.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using NLog;
+
+namespace PandoraMusicBox.MediaPortalPlugin.Tools {
+    internal class SettingAttribute : Attribute { }
+
+    internal class BaseSettings {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        
+        public string FileName {
+            get;
+            protected set;
+        }
+
+        public string Namespace {
+            get;
+            protected set;
+        }
+
+        public void LoadSettings() {
+            logger.Info("Loading Settings");
+            logger.Debug("Settings File: " + GetFileFullPath());
+            try {
+                using (MediaPortal.Profile.Settings xmlreader = new MediaPortal.Profile.Settings(GetFileFullPath())) {
+                    // loop through each property in the class
+                    foreach (PropertyInfo currProperty in GetType().GetProperties())
+                        foreach (object currAttr in currProperty.GetCustomAttributes(true))
+                            if (currAttr.GetType() == typeof(SettingAttribute)) {
+                                // we have found a property with the SettingAttribute applied
+                                if (currProperty.PropertyType == typeof(string)) {
+                                    string value = xmlreader.GetValueAsString(Namespace, currProperty.Name, "");
+                                    currProperty.GetSetMethod().Invoke(this, new object[] { value });
+                                }
+
+                                break;
+                            }
+                }
+            }
+            catch (Exception) {
+            }
+        }
+
+        public void SaveSettings() {
+            logger.Info("Saving Settings");
+            logger.Debug("Settings File: " + GetFileFullPath());
+
+            try {
+                using (MediaPortal.Profile.Settings xmlwriter = new MediaPortal.Profile.Settings(GetFileFullPath(), false)) {
+                    // loop through each property in the class
+                    foreach (PropertyInfo currProperty in GetType().GetProperties())
+                        foreach (object currAttr in currProperty.GetCustomAttributes(true))
+                            if (currAttr.GetType() == typeof(SettingAttribute)) {
+                                // we have found a property with the SettingAttribute applied
+                                if (currProperty.PropertyType == typeof(string)) {
+                                    object value = currProperty.GetGetMethod().Invoke(this, new object[] { });
+                                    xmlwriter.SetValue(Namespace, currProperty.Name, value);
+                                }
+
+                                break;
+                            }
+                }
+            }
+            catch (Exception) {
+
+            }
+        }
+
+        private string GetFileFullPath() {
+            return MediaPortal.Configuration.Config.GetFile(MediaPortal.Configuration.Config.Dir.Config, FileName);
+        }
+    }
+}
