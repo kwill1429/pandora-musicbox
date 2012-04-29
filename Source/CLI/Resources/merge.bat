cedb72feeac4c8a89490f3eef88232a8b5ba21d1
conrad.john <conrad.john@bb047c8b-32c9-1db6-8714-e51fcf91b571>
2012-04-29 23:44:50 +0000

    * fixed merge.bat issue. * pushed update.xml for MPEI publishing

--- Source/CLI/Resources/merge.bat
+++ Source/CLI/Resources/merge.bat
@@ -2,7 +2,7 @@
 
 md tmp
 :: add following for .NET 4.0:  /targetplatform:v4,C:\Windows\Microsoft.NET\Framework\v4.0.30319
-ilmerge  /out:tmp\pandora.exe pandora.exe PandoraMusicBox.Engine.dll PandoraMusicBox.Player.dll DirectShowLib-2005.dll
+ilmerge  /out:tmp\pandora.exe pandora.exe PandoraMusicBox.Engine.dll PandoraMusicBox.Player.dll DirectShowLib-2005.dll Newtonsoft.Json.dll
 
 IF EXIST pandora_UNMERGED.exe del pandora_UNMERGED.exe
 ren pandora.exe pandora_UNMERGED.exe
