3ce3118ba325681c7db68e4f7672254efa2c2865
conrad.john <conrad.john@bb047c8b-32c9-1db6-8714-e51fcf91b571>
2012-04-30 02:20:07 +0000

    tag for cli 0.7.0

---
+++ 0.7/0.7.0/Source/MediaPortalPlugin/MusicBoxSettings.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PandoraMusicBox.Engine.Data;
+using System.Reflection;
+using PandoraMusicBox.MediaPortalPlugin.Tools;
+using PandoraMusicBox.Engine.Encryption;
+
+namespace PandoraMusicBox.MediaPortalPlugin {
+
+    internal class MusicBoxSettings: BaseSettings {
+        BlowfishCipher cipher = new BlowfishCipher(PandoraCryptKeys.PW);
+
+        public MusicBoxSettings() {
+            FileName = "musicbox.xml";
+            Namespace = "PandoraMusicBox";
+        }
+
+        #region Settings
+
+        [Setting]
+        public string UserName {
+            get;
+            set;
+        }
+
+        [Setting]
+        public string EncryptedPassword {
+            get;
+            set;
+        }
+
+        public string Password {
+            get {
+                try { return cipher.Decrypt(EncryptedPassword); }
+                catch (Exception) {}
+                return "";
+            }
+            set {
+                if (value != null) EncryptedPassword = cipher.Encrypt(value);
+            }
+        }
+
+        [Setting]
+        public string LastStationId {
+            get;
+            set;
+        }
+
+        public PandoraStation LastStation {
+            get {
+                PandoraUser user = MusicBoxCore.Instance.MusicBox.User;
+                if (user == null) return null;
+                return GetStation(MusicBoxCore.Instance.MusicBox.AvailableStations, LastStationId);
+            }
+            set {
+                LastStationId = value.Id;
+            }
+        }
+
+        public string SadTrombone {
+            get;
+            internal set;
+        }
+
+        #endregion
+
+        #region Helper Methods
+
+        public PandoraStation GetStation(IList<PandoraStation> stations, string stationID) {
+            foreach (PandoraStation currStation in stations)
+                if (currStation.Id == stationID)
+                    return currStation;
+
+            return null;
+        }
+
+        #endregion
+    }
+}
---
+++ 0.7/0.7.0/Source/TrayApp/MusicBoxTray.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+using TrayApp.Properties;
+
+namespace TrayApp {
+    public class MusicBoxTray: Form {
+        protected NotifyIcon trayIcon;
+        protected ContextMenuStrip trayMenu;
+
+        public MusicBoxTray() {
+            // Create a simple tray menu with only one item.
+            trayMenu = new ContextMenuStrip();
+
+            Panel p = new Panel();
+            Label l = new Label();
+            l.Text = "SONG PANEL";
+            l.AutoSize = false;
+            l.Width = 500;
+            p.Controls.Add(l);
+
+            trayMenu.Items.Add(new ToolStripControlHost(p));
+            trayMenu.Items.Add(new ToolStripSeparator());
+            trayMenu.Items.Add("Login", null, OnLogin);
+            trayMenu.Items.Add(new ToolStripSeparator());
+            trayMenu.Items.Add("Exit", null, OnExit);
+
+            // Create a tray icon. In this example we use a
+            // standard system icon for simplicity, but you
+            // can of course use your own custom icon too.
+            trayIcon = new NotifyIcon();
+            trayIcon.Text = "Pandora MusicBox";
+            trayIcon.Icon = Resources.PandoraLogo;
+
+            // Add menu to tray icon and show it.
+            trayIcon.ContextMenuStrip = trayMenu;
+            trayIcon.Visible = true;
+        }
+
+        protected override void OnLoad(EventArgs e) {
+            Visible = false;
+            ShowInTaskbar = false;
+            base.OnLoad(e);
+        }
+
+        protected void OnExit(object sender, EventArgs e) {
+            Application.Exit();
+        }
+        
+        protected void OnLogin(object sender, EventArgs e) {
+            MessageBox.Show("login");
+        }
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main() {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MusicBoxTray());
+        }
+
+        private void InitializeComponent() {
+            this.SuspendLayout();
+            // 
+            // MusicBoxTray
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Name = "MusicBoxTray";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.ResumeLayout(false);
+
+        }
+
+    }
+}
