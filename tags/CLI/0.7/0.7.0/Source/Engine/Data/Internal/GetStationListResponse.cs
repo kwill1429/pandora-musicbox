c2ed8fea05a6f4512c7e997e8476f345ec9251d9
conrad.john <conrad.john@bb047c8b-32c9-1db6-8714-e51fcf91b571>
2012-04-30 02:20:07 +0000

    tag for cli 0.7.0

---
+++ tags/CLI/0.7/0.7.0/Source/Engine/Data/Internal/GetStationListResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace PandoraMusicBox.Engine.Data.Internal {
+    internal class GetStationListResponse: PandoraData {
+
+        [JsonProperty(PropertyName = "stations")]
+        public List<PandoraStation> Stations {
+            get;
+            set;
+        }
+
+
+        [JsonProperty(PropertyName = "deviceModel")]
+        public string Checksum {
+            get;
+            set;
+        }
+    }
+}
---
+++ tags/CLI/0.7/0.7.0/Source/Engine/Data/PandoraStation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using Newtonsoft.Json;
+
+namespace PandoraMusicBox.Engine.Data {
+    public class PandoraStation : PandoraData {
+        [JsonProperty(PropertyName = "stationName")]
+        public string Name {
+            get;
+            internal set;
+        }
+
+        [JsonProperty(PropertyName = "stationId")]
+        public string Id {
+            get;
+            internal set;
+        }
+
+        [JsonProperty(PropertyName = "stationToken")]
+        public string Token {
+            get;
+            internal set;
+        }
+
+        [JsonProperty(PropertyName = "isQuickMix")]
+        public bool IsQuickMix {
+            get;
+            internal set;
+        }
+        
+    }
+}
