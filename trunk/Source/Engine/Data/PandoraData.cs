6598480e0a15a3b7a6afd3e76c811ed776730812
conrad.john <conrad.john@bb047c8b-32c9-1db6-8714-e51fcf91b571>
2012-04-30 03:03:06 +0000

    * refactored request and response namespaces * added several new params to userLogin.

--- trunk/Source/Engine/Data/PandoraData.cs
+++ trunk/Source/Engine/Data/PandoraData.cs
@@ -4,51 +4,6 @@ using System.Text;
 using System.Xml;
 
 namespace PandoraMusicBox.Engine.Data {
-    public class PandoraData {
-
-        public string Json {
-            get;
-            internal set;
-        }
-
-        public Dictionary<string, string> Variables {
-            get;
-            protected set;
-        }
-        
-        // delete below
-        public string this[string key] {
-            get {
-                if (!Variables.ContainsKey(key)) 
-                    throw new PandoraException("XML-RPC response missing expected value: '" + key + "'");
-
-                return Variables[key];
-            }
-        }
-
-        public bool ContainsKey(string key) {
-            return Variables.ContainsKey(key);
-        }
-
-        internal static Dictionary<string, string> GetVariables(string xmlStr) {
-            XmlDocument xml = new XmlDocument();
-            xml.LoadXml(xmlStr);
-            return GetVariables(xml.SelectSingleNode("//struct"));
-        }
-
-        internal static Dictionary<string, string> GetVariables(XmlNode xml) {
-            Dictionary<string, string> lookup = new Dictionary<string, string>();
-            
-            try {
-                foreach (XmlNode currNode in xml.SelectNodes("member")) 
-                    lookup.Add(currNode["name"].InnerText, currNode["value"].InnerText);                
-            }
-            catch (Exception e) {
-                throw new PandoraException("Failed to parse response XML.", e, xml.OuterXml);
-            }
-
-            return lookup;
-        }
-
+    public abstract class PandoraData {
     }
 }
