7e2cf15098972416c9685d39ca344748655f599a
conrad.john <conrad.john@bb047c8b-32c9-1db6-8714-e51fcf91b571>
2012-04-30 02:19:14 +0000

    tag for 1.3.1

---
+++ tags/MediaPortalPlugin/1.3/1.3.1/Source/Engine/Data/PandoraUser.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using PandoraMusicBox.Engine.Encryption;
+using Newtonsoft.Json;
+
+namespace PandoraMusicBox.Engine.Data {
+    public enum AccountType { BASIC, TRIAL, PREMIUM, EXPIRED_SUBSCRIBER }
+
+    [JsonObject(MemberSerialization.OptIn)]
+    public class PandoraUser: PandoraData {
+        BlowfishCipher cipher = new BlowfishCipher(PandoraCryptKeys.PW);
+        
+        [JsonProperty(PropertyName = "username")]
+        public string Name {
+            get;
+            internal set;
+        }
+
+        internal string EncryptedPassword {
+            get;
+            set;
+        }
+
+        internal string Password {
+            get {
+                try { return cipher.Decrypt(EncryptedPassword); }
+                catch (Exception) { }
+                return "";
+            }
+            set {
+                if (value != null) EncryptedPassword = cipher.Encrypt(value);
+            }
+        }
+
+        [JsonProperty(PropertyName = "canListen")]
+        public bool CanListen {
+            get;
+            internal set;
+        }
+
+        [JsonProperty(PropertyName = "userAuthToken")]
+        public string AuthorizationToken {
+            get;
+            internal set;
+        }
+
+        [JsonProperty(PropertyName = "userId")]
+        public string Id {
+            get;
+            internal set;
+        }
+
+        [JsonProperty(PropertyName = "hasAudioAds")]
+        public bool RequiresAds {
+            get;
+            internal set;
+        }
+
+        public int AdInterval {
+            get;
+            internal set;
+        }
+
+        public AccountType AccountType {
+            get;
+            internal set;
+        }
+
+        [JsonProperty(PropertyName = "age")]
+        public int Age {
+            get;
+            set;
+        }
+
+        [JsonProperty(PropertyName = "zip")]
+        public int ZipCode {
+            get;
+            internal set;
+        }
+
+        public TimeSpan TimeoutInterval {
+            get {
+                if (this.AccountType == AccountType.BASIC)
+                    return new TimeSpan(1, 0, 0);
+                else
+                    return new TimeSpan(5, 0, 0);
+            }
+        }
+    }
+}
